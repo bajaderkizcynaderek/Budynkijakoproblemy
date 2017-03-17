67bcf5c271b571d8dcb633474405600de9fef0fe
bajaderkizcynaderek <dz@MacBook-Pro-dz.local>
2017-03-17 13:08:55 +0100

    światło poranka

---
+++ Budynkijakoproblemy/modelaplikacji.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ZarzadzanieZmiana.model
+{
+	[XmlRoot]
+	public class ModelAplikacji
+	{
+		public Budynkimodel()
+		{
+		}
+	}
+}
---
+++ Budynkijakoproblemy/programglowny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZarzadzanieZmiana.model;
+
+
+namespace ZarzadzanieZmiana
+{
+	class MainClass
+	{
+
+
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
