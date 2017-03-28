ceba326a04afb75556628ac8be103d7ae5d81173
bajaderkizcynaderek <dz@MacBook-Pro-dz.local>
2017-03-28 12:36:03 +0200

    rozmontujmy to w pizdu :)

---
+++ Budynkijakoproblemy/Input.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using ObamaWantsChange.model;
+
+namespace ObamaWantsChange
+{
+	public static class Input
+	{
+		public Input()
+		{
+		}
+		public static void Wait()
+		{
+			String command = System.Console.ReadLine();
+
+
+			if (command.Equals("help"))
+			{
+				Render.Rend(Txt.NO_HELP_BITCH);
+			}
+
+			else if (command.StartsWith("add"))
+			{
+				/*
+				bool nullChecker = (String.IsNullOrEmpty(command.Split(' ')[1]));
+				if (!nullChecker)
+					// hehe hihi
+				{
+					String subCommand = command.Split(' ')[1];
+					if (subCommand.Equals("client"))
+					{
+					}
+					// tu bedzie model.addclient....
+
+					// no i co. klient to skomplikowana lista danych, w00t w00t nawet nie wiem jaki to typ
+					// jak napisac entry interface do tego lols.
+					// ide robic hello worldy
+
+					//model.AddClient(clientName);
+					// SingleClient client = new SingleClient();
+					// client.AddCampaign(new SingleCampaign());
+					// model.AddClient(client);
+
+				}
+				else
+				{
+					System.Console.WriteLine(Txt.YOU_FORGOT);
+					return;
+				}
+				*/
+			}
+			else if (command.StartsWith("save"))
+			{
+				String fileName = command.Split(' ')[1];
+
+				model.Save(fileName);
+			}
+
+			// Pan wybaczy, probujemy rzucac gownem w sciane
+
+			else if (command.Equals("test"))
+			{
+
+				model.AddTheShit();
+			}
+			else if (command.Equals("list"))
+			{
+
+				model.ListTheList();
+			}
+			else if (command.Equals("navi"))
+			{
+
+				model.Navigate();
+			}
+
+			else if (command.StartsWith("load"))
+			{
+				String fileName = command.Split(' ')[1];
+
+				model.Load(fileName);
+			}
+			else if (command.Equals("exit"))
+			{
+				Stop();
+			}
+
+
+			// koniec przewalenia
+
+		}
+	}
+}
---
+++ Budynkijakoproblemy/Render.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using ObamaWantsChange.model;
+
+namespace ObamaWantsChange
+{
+	public static class Render
+	{
+		public static void Rend(String message)
+		{
+			Console.WriteLine(message);
+		}
+		public static void Rendmany( String [] messages)
+		{
+			foreach (String message in messages)
+			{
+				Console.WriteLine(message);
+			}  
+		}
+	}
+
+}
